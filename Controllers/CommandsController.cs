846299ec182fe86f51e172eee27c89f556298bca
mohammedbabelly <mohammedababelly@gmail.com>
2020-10-23 22:18:19 +0300

    Add project files.

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repo;
+
+        public CommandsController(ICommanderRepo repo)
+        {
+            _repo = repo;
+        }
+        //private readonly MockCommanderRepo _repo = new MockCommanderRepo();
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GelAllCommands()
+        {
+            return Ok(_repo.GetAppCommands());
+        }
+        //GET api/command/5
+        [HttpGet("{id}")]
+        public ActionResult <Command> GelCommandById(int id)
+        {
+            return Ok(_repo.GetCommandById(id));
+        }
+      
+    }
+}
---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,28 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            IEnumerable < Command >commands= new List<Command> 
+             {
+                new Command { Id = 1, HowTo = "Write a code", Line = "Keyboard", Platform = "Programming" },
+                 new Command { Id = 2, HowTo = "Write a code", Line = "Keyboard", Platform = "Programming" },
+                 new Command { Id = 3, HowTo = "Write a code", Line = "Keyboard", Platform = "Programming" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            Command command = new Command { Id=1,HowTo="Write a code",Line="Keyboard",Platform="Programming"};
+            return command;
+        }
+    }
+}
